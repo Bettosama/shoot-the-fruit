7bdfd812435ffcbadb8b31294849452df7c6f49f
Batool Alsalem <bettosama10@gmail.com>
2022-02-10 17:01:25 +0300

    commit

---
+++ shootTheFruit.py
@@ -0,0 +1,14 @@
+import pgzrun
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = 300
+    apple.y = 200
+
+place_apple()
+pgzrun.go()
